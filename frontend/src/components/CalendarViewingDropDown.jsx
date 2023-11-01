4e2883d68cd0c2ac16c7289798111e050e8e3328
Afsha10 <unityofbeing1@gmail.com>
2023-11-01 23:41:07 +0000

    Update CalendarViewingDropDown.jsx
    * Make some styling changes

--- frontend/src/components/CalendarViewingDropDown.jsx
+++ frontend/src/components/CalendarViewingDropDown.jsx
@@ -3,12 +3,22 @@ import { Listbox } from "@headlessui/react";
 function CalendarViewingDropdown({ calendarType, setCalendarType }) {
   return (
     <Listbox value={calendarType} onChange={setCalendarType}>
-      <Listbox.Button><span className="ml-5">Select a Calendar View</span></Listbox.Button>
+      <Listbox.Button>
+        <span className="text-white text-lg sm:text-xl">
+          Select a Calendar View
+        </span>
+      </Listbox.Button>
       <Listbox.Options>
-        <Listbox.Option value="monthly">
+        <Listbox.Option
+          className="text-white text-base sm:text-lg"
+          value="monthly"
+        >
           Monthly Calendar View {calendarType === "monthly" && "✔️"}
         </Listbox.Option>
-        <Listbox.Option value="four-weekly">
+        <Listbox.Option
+          className="text-white text-base sm:text-lg"
+          value="four-weekly"
+        >
           Four-Weekly Calendar View {calendarType === "four-weekly" && "✔️"}
         </Listbox.Option>
       </Listbox.Options>
