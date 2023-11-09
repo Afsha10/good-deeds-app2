7dc7163dca24164c72101c694ea8dee4296b14a4
Afsha10 <unityofbeing1@gmail.com>
2023-11-09 17:14:48 +0000

    Connect the cancel button with backend using fetch
    * Connect the cancel button to delete the booking from the database using fetch data
    * It is currently not deleting the booking when the cancel button is clicked the first time

--- frontend/src/components/CancelBookingButton.jsx
+++ frontend/src/components/CancelBookingButton.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { baseUrl } from "../config";
 // import ConfirmationDialogueBoxTom from "./ConfirmationDialogueBoxTom";
 
 function CancelBookingButton({ selectedSession, allSessions, setSessions }) {
@@ -6,6 +7,20 @@ function CancelBookingButton({ selectedSession, allSessions, setSessions }) {
     const updatedSessions = allSessions.map((session) => {
       if (session.session_id === selectedSession.session_id) {
         session.volunteer_id = null;
+        console.log("session->", session);
+        fetch(`${baseUrl}/bookings/${session.booking_id}`, {
+          method: "delete",
+        })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Failed to cancel your booking");
+            } else {
+              console.log(
+                `Booking with id ${selectedSession.session_id} is cancelled`
+              );
+            }
+          })
+          .catch((error) => console.log("Error cancelling booking", error));
       }
       return session;
     });
