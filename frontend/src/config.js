1215bb6c6ebb12cd7cd281370e543fa56eddc39a
Afsha10 <unityofbeing1@gmail.com>
2023-12-11 10:33:34 +0000

    Refactor baseUrl in config.js so that it is dynamic

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,4 @@
-export const baseUrl = "https://afsha-good-deeds-app.onrender.com"; // prod
-
-// export const baseUrl = "http://localhost:5000"; //dev
+export const baseUrl =
+  process.env.REACT_APP_IS_PRODUCTION === "true"
+    ? "https://afsha-good-deeds-app.onrender.com"
+    : "http://localhost:5000";
