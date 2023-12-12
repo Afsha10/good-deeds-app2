eba724824d34f9c9263b72d8fb2415e7de19fa5a
Afsha10 <unityofbeing1@gmail.com>
2023-12-12 23:26:16 +0000

    Make the calendar selection drop down not get cut off
    Make the calendar selection drop down not get cut off because it is hard for users to read it
    * To correct it give some left margin for the drop down and the selection options

--- frontend/src/components/CalendarViewingDropDown.jsx
+++ frontend/src/components/CalendarViewingDropDown.jsx
@@ -3,13 +3,13 @@ import { Listbox } from "@headlessui/react";
 function CalendarViewingDropdown({ calendarType, setCalendarType }) {
   return (
     <Listbox value={calendarType} onChange={setCalendarType}>
-      <Listbox.Button className="relative">
+      <Listbox.Button className="relative ml-6">
         <span className="text-white text-lg sm:text-xl">
           Select a Calendar View
         </span>
         <span className="absolute pl-2">🔽</span>
       </Listbox.Button>
-      <Listbox.Options>
+      <Listbox.Options className="ml-6">
         <Listbox.Option
           className="text-white text-base sm:text-lg"
           value="monthly"
